e681b5785b11e872da2c4c41ddb93f1ad58b2263
abc <1741785576@qq.com>
2019-12-25 12:07:01 +0800

    ...

--- app/src/main/java/com/example/peiwang/AddDeviceActivity.java
+++ app/src/main/java/com/example/peiwang/AddDeviceActivity.java
@@ -1,18 +1,41 @@
 package com.example.peiwang;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.example.base.BaseActivity;
-import com.example.base.BaseAdapter;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.DecodeHintType;
+import com.orhanobut.logger.Logger;
 
-public class AddDeviceActivity extends BaseActivity {
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.bingoogolapple.qrcode.core.BarcodeType;
+import cn.bingoogolapple.qrcode.core.QRCodeView;
+import cn.bingoogolapple.qrcode.zxing.ZXingView;
+
+public class AddDeviceActivity extends BaseActivity implements QRCodeView.Delegate {
+    private static final int REQUEST_CODE_CHOOSE_QRCODE_FROM_GALLERY = 666;
+    @BindView(R.id.zxingview)
+    ZXingView mZXingView;
+
     @Override
     protected int getContentViewId() {
-        return 0;
+        return R.layout.activity_addevice;
     }
 
     @Override
     protected void getUnbinder() {
+        ButterKnife.bind(this);
 
     }
 
@@ -23,6 +46,80 @@ public class AddDeviceActivity extends BaseActivity {
 
     @Override
     protected void disarmState() {
+
+    }
+
+    @Override
+    public void onScanQRCodeSuccess(String result) {
+        Logger.d("result:" + result);
+        vibrate();
+        mZXingView.startSpot(); // 开始识别
+    }
+
+    @Override
+    public void onCameraAmbientBrightnessChanged(boolean isDark) {
+        // 这里是通过修改提示文案来展示环境是否过暗的状态，接入方也可以根据 isDark 的值来实现其他交互效果
+        String tipText = mZXingView.getScanBoxView().getTipText();
+        String ambientBrightnessTip = "\n环境过暗，请打开闪光灯";
+        if (isDark) {
+            if (!tipText.contains(ambientBrightnessTip)) {
+                mZXingView.getScanBoxView().setTipText(tipText + ambientBrightnessTip);
+            }
+        } else {
+            if (tipText.contains(ambientBrightnessTip)) {
+                tipText = tipText.substring(0, tipText.indexOf(ambientBrightnessTip));
+                mZXingView.getScanBoxView().setTipText(tipText);
+            }
+        }
+    }
+
+    @Override
+    public void onScanQRCodeOpenCameraError() {
+        Logger.d( "打开相机出错");
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mZXingView.startCamera(); // 打开后置摄像头开始预览，但是并未开始识别
+        Map<DecodeHintType, Object> hintMap = new EnumMap<>(DecodeHintType.class);
+        List<BarcodeFormat> formatList = new ArrayList<>();
+        formatList.add(BarcodeFormat.QR_CODE);
+        formatList.add(BarcodeFormat.UPC_A);
+        formatList.add(BarcodeFormat.EAN_13);
+        formatList.add(BarcodeFormat.CODE_128);
+        hintMap.put(DecodeHintType.POSSIBLE_FORMATS, formatList); // 可能的编码格式
+        hintMap.put(DecodeHintType.TRY_HARDER, Boolean.TRUE); // 花更多的时间用于寻找图上的编码，优化准确性，但不优化速度
+        hintMap.put(DecodeHintType.CHARACTER_SET, "utf-8"); // 编码字符集
+        mZXingView.setType(BarcodeType.CUSTOM, hintMap); // 自定义识别的类型
+
+        mZXingView.startSpotAndShowRect(); // 显示扫描框，并开始识别
+//        mZXingView.startCamera(Camera.CameraInfo.CAMERA_FACING_FRONT); // 打开前置摄像头开始预览，但是并未开始识别
+        mZXingView.startSpotAndShowRect(); // 显示扫描框，并开始识别
+    }
+
+    @Override
+    protected void onStop() {
+        mZXingView.stopCamera(); // 关闭摄像头预览，并且隐藏扫描框
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        mZXingView.onDestroy(); // 销毁二维码扫描控件
+        super.onDestroy();
+    }
+
+    private void vibrate() {
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vibrator.vibrate(200);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mZXingView.startSpotAndShowRect(); // 显示扫描框，并开始识别
+
 
     }
 }
