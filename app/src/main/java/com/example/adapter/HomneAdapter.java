fb7e74b98de352b196754ff077d835d4823f170f
abc <1741785576@qq.com>
2020-01-02 18:09:24 +0800

    Merge branch 'master' of https://github.com/MyNameIsAbc/Peiwang
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/adapter/HomneAdapter.java
+++ app/src/main/java/com/example/adapter/HomneAdapter.java
@@ -2,8 +2,10 @@ package com.example.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -25,7 +27,7 @@ public class HomneAdapter extends BaseAdapter<DeviceBean> {
 
     @Override
     public RecyclerView.ViewHolder onCreate(ViewGroup parent, int viewType) {
-        View view = layoutInflater.inflate(R.layout.layout_device_list, null);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_device_list, parent, false);
         return new DeviceHolder(view);
     }
 
@@ -36,7 +38,6 @@ public class HomneAdapter extends BaseAdapter<DeviceBean> {
     }
 
 
-    static
     class DeviceHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.tv_device_sn)
         TextView tvDeviceSn;
@@ -46,12 +47,14 @@ public class HomneAdapter extends BaseAdapter<DeviceBean> {
         TextView tvDeviceFrom;
         @BindView(R.id.tv_device_to)
         TextView tvDeviceTo;
-        @BindView(R.id.tv_device_volume)
-        TextView tvDeviceVolume;
-        @BindView(R.id.tv_charge)
-        TextView tvCharge;
-        @BindView(R.id.tv_money)
-        TextView tvMoney;
+        @BindView(R.id.iv_status)
+        ImageView imageView;
+        @BindView(R.id.iv_power)
+        ImageView iv_power;
+        @BindView(R.id.tv_do)
+        TextView tv;
+        @BindView(R.id.tv_do2)
+        TextView tv2;
 
         DeviceHolder(View view) {
             super(view);
@@ -59,15 +62,44 @@ public class HomneAdapter extends BaseAdapter<DeviceBean> {
         }
 
         public void setData(DeviceBean statusBean) {
-            tvDeviceSn.setText("设备SN:" + statusBean.getSn());
-            if (statusBean.getAutoLang() == 1)
-                tvDeviceName.setText("翻译模式:自动切换语言");
-            else
-                tvDeviceName.setText("翻译模式:固定语言");
-            tvCharge.setText("电量:" + statusBean.getPower());
-            tvDeviceVolume.setText("音量:" + statusBean.getVolume());
-            Constant.setLngUI(statusBean.getFromLang(),tvDeviceFrom,null);
-            Constant.setLngUI(statusBean.getToLang(),tvDeviceTo,null);
+            if (statusBean.getNetwork() == -1) {
+                tvDeviceSn.setText("设备SN:" + statusBean.getSn());
+                tvDeviceSn.setTextColor(context.getResources().getColor(R.color.colorglay));
+                tvDeviceName.setText("模式:已离线");
+                tvDeviceName.setTextColor(context.getResources().getColor(R.color.colorglay));
+                tvDeviceFrom.setTextColor(context.getResources().getColor(R.color.colorglay));
+                tvDeviceTo.setTextColor(context.getResources().getColor(R.color.colorglay));
+                imageView.setImageResource(R.mipmap.ic_wifi_0);
+                tv.setTextColor(context.getResources().getColor(R.color.colorglay));
+                tv2.setTextColor(context.getResources().getColor(R.color.colorglay));
+            } else {
+                tvDeviceSn.setText("设备SN:" + statusBean.getSn());
+                tvDeviceSn.setTextColor(context.getResources().getColor(android.R.color.black));
+                if (statusBean.getAutoLang() == 1)
+                    tvDeviceName.setText("模式:自动");
+                else
+                    tvDeviceName.setText("模式:固定");
+                tvDeviceName.setTextColor(context.getResources().getColor(android.R.color.black));
+                Constant.setLngUI(statusBean.getFromLang(), tvDeviceFrom, null);
+                Constant.setLngUI(statusBean.getToLang(), tvDeviceTo, null);
+                tvDeviceFrom.setTextColor(context.getResources().getColor(android.R.color.black));
+                tvDeviceTo.setTextColor(context.getResources().getColor(android.R.color.black));
+                if (statusBean.getNetwork() == 1)
+                    imageView.setImageResource(R.mipmap.ic_wifi_1);
+                else if (statusBean.getNetwork() == 2)
+                    imageView.setImageResource(R.mipmap.ic_wifi_2);
+                else if (statusBean.getNetwork() == 3)
+                    imageView.setImageResource(R.mipmap.ic_wifi_3);
+
+                if (statusBean.getPower() == 1)
+                    iv_power.setImageResource(R.mipmap.power_1);
+                else if (statusBean.getPower() == 2)
+                    iv_power.setImageResource(R.mipmap.power_2);
+                else if (statusBean.getPower() == 3)
+                    iv_power.setImageResource(R.mipmap.power_3);
+                tv.setTextColor(context.getResources().getColor(R.color.colorText));
+                tv2.setTextColor(context.getResources().getColor(R.color.colorText));
+            }
         }
     }
 
